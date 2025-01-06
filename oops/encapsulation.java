1cf67433f352bdb8a5bdeb88d9e7163f7cbd6052
Dhruvik-chavda <dhruvik@satvasolutions.com>
2025-01-06 10:09:18 +0530

    oops encapsulation practice

---
+++ oops/encapsulation.java
@@ -0,0 +1,56 @@
+
+public class Person {
+
+    
+    private String name;
+    private int age;
+
+    
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+   
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    public int getAge() {
+        return age;
+    }
+
+   
+    public void setAge(int age) {
+       
+        if(age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Age must be positive.");
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Person person = new Person("John", 25);
+
+       
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+
+       
+        person.setAge(30);
+        System.out.println("Updated Age: " + person.getAge());
+
+       
+        person.setAge(-5);  
+    }
+}
